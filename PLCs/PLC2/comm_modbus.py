438382f819c1093131fd6f3ca228e0f90d84e128
marcolucc <marcolucchese0@gmail.com>
2023-03-17 10:58:30 +0100

    Initial Commit

--- PLCs/PLC2/comm_modbus.py
+++ PLCs/PLC2/comm_modbus.py
@@ -7,36 +7,30 @@ stop_flag1 = False
 stop_flag2 = False
 
 while (not stop_flag1):
-    #print("connessione plc1")
     try:
-        plc1 = easymodbus.modbusClient.ModbusClient('plc1.rete', 502)
+        plc1 = easymodbus.modbusClient.ModbusClient('plc1.net', 502)
         plc1.connect()
         stop_flag1 = True
     except:
-        print("errore connessione plc1")
+        print("error connection plc1")
         time.sleep(1)
 
 while (not stop_flag2):
-    #print("connessione plc2")
     try:
         plc2 = easymodbus.modbusClient.ModbusClient('localhost', 502)
         plc2.connect()
         stop_flag2 = True
     except:
-        print("errore connessione plc2")
+        print("error connection plc2")
         time.sleep(1)
 
 
 while (True):
-    #print("leggo")
-    #leggo i registri di input (level e request)
     
     try:
-        #print("leggo da plc2")
         inputRegisters = plc2.read_coils(0, 1)
-        #print(inputRegisters)
     except:
-        print("Errore in lettura coil")
+        print("coil reading error")
 
     try:
         #print("\n Richiesta: ")
@@ -44,17 +38,13 @@ while (True):
     
         print(richiesta)
     except:
-        print("errore lettura buffer registro")
+        print("buffer reading error")
     
-
-    #scrivo i coil di richiesta
-    #print("Scrivo su plc1")
     try:
         plc1.write_single_coil(2, richiesta)
 
         plc1.read_coils(2, 1)
     except:
-        print("errore scrittura")
-    #print("close connection")
+        print("writing error")
     time.sleep(1)
     
